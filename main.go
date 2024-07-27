a290e947312ff25d8cf72b05e53f1e53360e4866
eymardfreiredev <eymardfreire@gmail.com>
2024-07-27 11:24:14 -0400

    Set up file server

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Use FileServer to serve static files
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileServer)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
+	log.Println("Starting server on :8080")
 	// Start the server
-	server.ListenAndServe()
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
